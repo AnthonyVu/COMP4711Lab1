39f9b4b758860b257f4a10adbbdd75f5920d27cd
Anthony Vu <anthony_vu@live.ca>
2018-01-12 08:18:51 -0800

    Added comments

--- Student.php
+++ Student.php
@@ -7,10 +7,46 @@
  */
 
 /**
- * Description of Student
+ * Student Class
  *
  * @author Anthony Vu
  */
 class Student {
     //put your code here
+    
+    // Constructor for Student Class
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    
+    // Add email
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+
+    // Add grade
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+    
+    // Returns the average grade of a student
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    // Returns a string that displays a student's name, average, and contact information
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
 }
